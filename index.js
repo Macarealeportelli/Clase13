66cbc1e6b0b2a099d301a682b84ca4db93298bc5
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-08-10 20:36:59 -0300

    agregue espaciado entre las rtas del usuario en alert

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ let nombre = prompt ("Decime tu nombre ")
 
 let apellido = prompt ("Decime tu Apellido")
 
-alert ("Hola  " + nombre + apellido + " , Bienvenida a ADA!")
+alert ("Hola  " + nombre + " " + apellido + " , Bienvenida a ADA!")
